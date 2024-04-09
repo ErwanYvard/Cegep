2df33aa00b4f1e7b5de4e3078e47c339846f00f0
ErwanYvard <161734222+ErwanYvard@users.noreply.github.com>
2024-04-09 11:55:23 -0400

    Add files via upload

---
+++ Jardin/index.php
@@ -0,0 +1,24 @@
+<?php
+include ("inclus/entete.inc");
+?>
+<div style="text-align: center; margin: 10px">
+    Les jardins collectif d'Alma ont pour objectif de démontrer que l'on peut faire pousser de nombreuses plantes
+    nourricieres au Lac
+    afin d'éduquer sur la possibilité de pouvoir produire soit même sa nourriture.
+</div >
+
+    <div class="plante">
+        <a href="https://fr.wikipedia.org/wiki/Th%C3%A9_du_Labrador">
+            <img src="images/theLabrador.jpg" alt=""class="" height="400px" width="400px"></a>
+        </a>
+        <a href="https://fr.wikipedia.org/wiki/Tomate">
+            <img src="images/Fleurtomate.jpg" alt=""class="" height="400px" width="400px"></a>
+        </a>
+        <a href="https://fr.wikipedia.org/wiki/Basilic_(plante)">
+            <img src="images/basilic.jpg" alt=""class="" height="400px" width="400px"></a>
+        </a>
+    </div>
+
+<?php
+include ("inclus/pieddepage.inc");
+?>
